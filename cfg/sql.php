b5d47f30e45f659c2f9939a786ea35d3a73dcced
Thomas <Charlesworth>
2016-01-27 09:01:01 +0700

    mysql cfg

---
+++ cfg/sql.php
@@ -0,0 +1,8 @@
+<?
+		$hostname= "sql6.freemysqlhosting.net";
+		$username = "sql6104496";
+		$password = "GHwp9t2RyM";
+		$dbname = "sql6104496";
+
+		$mydb = mysqli_connect($hostname,$username,$password,$dbname) or die("There is Database connection error");
+?>
