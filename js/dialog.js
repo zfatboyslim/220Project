9f6c3ff2064150c0dc92be1673ed18d3db6a0513
Thiwa W <wangmeimoei@aol.com>
2016-02-01 10:43:04 +0700

    no message

---
+++ js/dialog.js
@@ -0,0 +1,20 @@
+      var login = document.getElementById('login'),
+      guest = document.getElementById('guest'),
+      signup = document.getElementById('signup'),
+      signupDialog = document.getElementById('signup-dialog'),
+      loginDialog = document.getElementById('login-dialog')
+
+  login.addEventListener('click', function() {
+    loginDialog.className = 'dialog visible'
+  })
+  guest.addEventListener('click', function() {
+    // TODO: redirect to home use window.location = url
+    console.log('go to ' + window.location.href + '/home')
+  })
+  signup.addEventListener('click', function() {
+    signupDialog.className = 'dialog visible'
+  })
+  function clearDialog() {
+    loginDialog.className = 'dialog'
+    signupDialog.className = 'dialog'
+  }
