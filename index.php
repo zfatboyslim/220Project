2530a27011334fea5e157a25b5c09d7aa0fb4904
Thiwa W <wangmeimoei@aol.com>
2016-02-01 12:53:32 +0700

    no message

--- index.php
+++ index.php
@@ -2,57 +2,64 @@
 <html>
 <head>
 	<meta charset="UTF-8" />
-     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-    
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<link rel="stylesheet" href="css/styles.css">
-	
+	<link rel="stylesheet" href="css/home.css">
 <!--	BREAKBREAKBREAK-->
 	<link rel="stylesheet" type="text/css" href="css/demo.css" />
 	<link rel="stylesheet" type="text/css" href="css/foundation.css" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <link rel="stylesheet" type="text/css" href="css/custom.css" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" href="css/custom.css" />
 	<script type="text/javascript" src="js/modernizr.custom.79639.js"></script>
-		<noscript>
-			<link rel="stylesheet" type="text/css" href="css/styleNoJS.css" />
-		</noscript>
+	<script type="text/javascript" src="js/dialog.js"></script>
+	<noscript>
+		<link rel="stylesheet" type="text/css" href="css/styleNoJS.css" />
+	</noscript>
 <!--	BREAKBREAKBREAK-->
-	
 	<title>index page</title>
 </head>
 <body>
 	<div id="navbar">
-<ul>
-	<li class='active'><a href='#'><span>About</span></a></li>
-	<li><a href='#'><span>Lifestyle</span></a></li>
-	<li><a href='#'><span>Food</span></a></li>
-	<li><a href='#'><span>Style</span></a></li>
-	<li><a href='#'><span>Inspiration</span></a></li>
-	<li><a href='#'><span>Health</span></a></li>
-	<li><a href='#'><span>Contact Us</span></a></li>
+		<ul>
+			<li class='active'><a href='#'><span>About</span></a></li>
+			<li><a href='#'><span>Promotion</span></a></li>
+			<li><a href='#'><span>Magazines</span></a></li>
+			<li><a href='#'><span>Contact Us</span></a></li>
+			<li><a href='#'><span>Your Cart</span></a></li>
 <!--<?php
 	//Do not delete
 	//require("controllers/checksession.php");
 ?>-->
-	<li class='last'><a href='#'><span>login / register</span></a></li>
-	<hr>
-</ul>
-		
-</div>
-	<!--BREAKBREAKBREAKBREAK-->
-<div class="container demo-2">
-		
-            <div id="slider" class="sl-slider-wrapper">
+			<li class='last' id="login"><a href='#'><span>login / register</span></a></li>
+			<hr>
+		</ul>
+	</div>
 
+	<!--this is a hidden dialog for registration-->
+	<div id="login-dialog" class="dialog">
+		<form class="dialog-form" action="index.php" method="post">
+	    <input type="text" name="email" placeholder="Your e-mail" />
+	    <input type="text" name="username" placeholder="Username" />
+	    <input type="password" name="password" placeholder="Password"/>
+	    <input type="password" name="confirm-password" placeholder="Confirm Password"/>
+	    <button type="submit" name="submit-signup">Agree</button>
+	    <button type="reset" name="reset" onclick="clearDialog()">Cancel</button>
+	  </form>
+	</div>
+	<!--end of the hidden dialog div-->
+
+	<!--BREAKBREAKBREAKBREAK-->
+	<div class="container demo-2">
+			<div id="slider" class="sl-slider-wrapper">
 				<div class="sl-slider">
-				
 					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
 						<div class="sl-slide-inner">
 							<div class="bg-img bg-img-1"></div>
-							<h2>A bene placito.</h2>
-							<blockquote><p>You have just dined, and however scrupulously the slaughterhouse is concealed in the graceful distance of miles, there is complicity.</p><cite>Ralph Waldo Emerson</cite></blockquote>
-						</div>
-					</div>
-					
+								<h2>A bene placito.</h2>
+								<blockquote><p>You have just dined, and however scrupulously the slaughterhouse is concealed in the graceful distance of miles, there is complicity.</p><cite>Ralph Waldo Emerson</cite></blockquote>
+							</div>
+				</div>
+
 					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
 						<div class="sl-slide-inner">
 							<div class="bg-img bg-img-2"></div>
@@ -60,7 +67,7 @@
 							<blockquote><p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p><cite>Albert Schweitzer</cite></blockquote>
 						</div>
 					</div>
-					
+
 					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
 						<div class="sl-slide-inner">
 							<div class="bg-img bg-img-3"></div>
@@ -68,7 +75,7 @@
 							<blockquote><p>Thousands of people who say they 'love' animals sit down once or twice a day to enjoy the flesh of creatures who have been utterly deprived of everything that could make their lives worth living and who endured the awful suffering and the terror of the abattoirs.</p><cite>Dame Jane Morris Goodall</cite></blockquote>
 						</div>
 					</div>
-					
+
 					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
 						<div class="sl-slide-inner">
 							<div class="bg-img bg-img-4"></div>
@@ -76,7 +83,7 @@
 							<blockquote><p>The human body has no more need for cows' milk than it does for dogs' milk, horses' milk, or giraffes' milk.</p><cite>Michael Klaper M.D.</cite></blockquote>
 						</div>
 					</div>
-					
+
 					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
 						<div class="sl-slide-inner">
 							<div class="bg-img bg-img-5"></div>
@@ -97,7 +104,7 @@
 			</div><!-- /slider-wrapper -->
 	</div>
 <!--BREAKBREAKBREAKBREAK-->
-	
+
 	<!--	BEGINNING OF PROMOTIONS-->
 		<div class="promotions">
 	<hr>
@@ -208,12 +215,12 @@
 	<script type="text/javascript" src="js/jsjs/jquery.min.js"></script>
 	<script type="text/javascript" src="js/jsjs/foundation.js"></script>
 	<script type="text/javascript" src="js/jsjs/jquery.flip.js"></script>
-	
+
 		<script type="text/javascript" src="js/jquery.ba-cond.min.js"></script>
 		<script type="text/javascript" src="js/jquery.slitslider.js"></script>
-		<script type="text/javascript">	
+		<script type="text/javascript">
 			$(function() {
-			
+
 				var Page = (function() {
 
 					var $nav = $( '#nav-dots > span' ),
@@ -229,28 +236,28 @@
 						init = function() {
 
 							initEvents();
-							
+
 						},
 						initEvents = function() {
 
 							$nav.each( function( i ) {
-							
+
 								$( this ).on( 'click', function( event ) {
-									
+
 									var $dot = $( this );
-									
+
 									if( !slitslider.isActive() ) {
 
 										$nav.removeClass( 'nav-dot-current' );
 										$dot.addClass( 'nav-dot-current' );
-									
+
 									}
-									
+
 									slitslider.jump( i + 1 );
 									return false;
-								
+
 								} );
-								
+
 							} );
 
 						};
@@ -262,22 +269,33 @@
 				Page.init();
 
 				/**
-				 * Notes: 
-				 * 
+				 * Notes:
+				 *
 				 * example how to add items:
 				 */
 
 				/*
-				
+
 				var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
-				
+
 				// call the plugin's add method
 				ss.add($items);
 
 				*/
-			
+
 			});
 		</script>
 	<script type="text/javascript" src="js/jsjs/app.js"></script>
+	<script type="text/javascript">
+	  var login = document.getElementById('login'),
+	      loginDialog = document.getElementById('login-dialog')
+
+	  login.addEventListener('click', function() {
+	    loginDialog.className = 'dialog visible'
+	  })
+	  function clearDialog() {
+	    loginDialog.className = 'dialog'
+	  }
+	</script>
 	</body>
 </html>
--- js/dialog.js
+++ js/dialog.js
@@ -7,14 +7,6 @@
   login.addEventListener('click', function() {
     loginDialog.className = 'dialog visible'
   })
-  guest.addEventListener('click', function() {
-    // TODO: redirect to home use window.location = url
-    console.log('go to ' + window.location.href + '/home')
-  })
-  signup.addEventListener('click', function() {
-    signupDialog.className = 'dialog visible'
-  })
   function clearDialog() {
     loginDialog.className = 'dialog'
-    signupDialog.className = 'dialog'
   }
